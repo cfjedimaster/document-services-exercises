09d4159b37afc9b2459ec66d3770515d0889cf72
Raymond Camden <raymondcamden@gmail.com>
2022-08-26 13:21:30 -0500

    work

--- README.md
+++ README.md
@@ -3,6 +3,8 @@
 A set of exercises related to [Adobe Document Services](https://developer.adobe.com/document-services/homepage/). These are all 
 Node.js based exercises and require credentials.
 
+Code is assumed to be run from this directory, so for example, `node optimize/ocr_pdf.js`.
+
 ## Optimize
 
 Exercices related to optimization functions for documents.
--- optimize/ocr_pdf.js
+++ optimize/ocr_pdf.js
@@ -1,1 +1,53 @@
+/*
+Required dependencies. 
+
+pdfSDK is our main doc services pdf
+fs is for file system stuff
+*/
 const pdfSDK = require('@adobe/pdfservices-node-sdk');
+const fs = require('fs');
+
+
+/*
+Defines our input and output, these would be dynamic normally...
+*/
+const input = './pdfs/pdf_that_needs_ocr.pdf';
+const output = './pdfs/pdf_that_is_ocr.pdf';
+
+/*
+ensure it actually exist...
+*/
+if(!fs.existsSync(input)) {
+	console.error(`Can't find input file ${input}`);
+	process.exit(1);
+}
+
+
+/*
+Clean up previous output if it exists. In a real environment, you have multiple alternatives you could use.
+For example, taking the filename and appending "_ocr". You could append a time stamp. A UUID. etc.
+*/
+if(fs.existsSync(output)) fs.unlinkSync(output);
+
+
+const credentials =  pdfSDK.Credentials
+	.serviceAccountCredentialsBuilder()
+	.fromFile('pdfservices-api-credentials.json')
+	.build();
+
+const executionContext = pdfSDK.ExecutionContext.create(credentials),
+	ocrOperation = pdfSDK.OCR.Operation.createNew();
+
+const inputPDF = pdfSDK.FileRef.createFromLocalFile(input);
+ocrOperation.setInput(inputPDF);
+
+ocrOperation.execute(executionContext)
+.then(result => result.saveAsFile(output))
+.catch(err => {
+	if(err instanceof pdfSDK.Error.ServiceApiError
+		|| err instanceof pdfSDK.Error.ServiceUsageError) {
+		console.log('Exception encountered while executing operation', err);
+	} else {
+		console.log('Exception encountered while executing operation', err);
+	}
+});
